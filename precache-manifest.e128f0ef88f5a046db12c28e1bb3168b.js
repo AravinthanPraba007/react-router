01647aab30757a66943fd4b95faf7c1cec3e0d61
Aravinthan Praba <799532>
2020-09-27 17:41:27 +0530

    Updates

---
+++ precache-manifest.e128f0ef88f5a046db12c28e1bb3168b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ee16566b023b9f92d08e724ffb92df7",
+    "url": "/react-router/index.html"
+  },
+  {
+    "revision": "2af857ff5a23b5fd4564",
+    "url": "/react-router/static/css/main.d1e2b0af.chunk.css"
+  },
+  {
+    "revision": "3cea9494ea85f63d0283",
+    "url": "/react-router/static/js/2.8416f7cb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-router/static/js/2.8416f7cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2af857ff5a23b5fd4564",
+    "url": "/react-router/static/js/main.faccbf77.chunk.js"
+  },
+  {
+    "revision": "30567b3474ec4e6b30b3",
+    "url": "/react-router/static/js/runtime-main.5a3f1648.js"
+  }
+]);
